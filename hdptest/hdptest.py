2fd0dffbfd1899f003b3c0ed58255e40b214fbc7
Ismael Figueroa <ifigueroap@gmail.com>
2019-10-22 16:17:08 -0300

    More fixes

--- hdptest/hdptest.py
+++ hdptest/hdptest.py
@@ -1,8 +1,8 @@
 from IPython.core.display import display, HTML
 
 def hdp_test(_fun, _input, _expected):
-    ok__message = '<span style="color: green;">[OK] Caso de prueba correcto para entrada {0}</span>'
-    err_message = '<span style="color: red;>[ERROR] Para entrada {0} se espera {1} pero se obtuvo {2}</span>'
+    ok__message = '<span style="color: green;"><b>[OK]</b> Caso de prueba correcto para entrada {0}</span>'
+    err_message = '<span style="color: red;><b>[ERROR]</b> Para entrada {0} se espera {1} pero se obtuvo {2}</span>'
     try:
         _res = _fun(_input)
         assert _res == _expected
