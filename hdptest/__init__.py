5305053a8f6cfe758343914c57e5d4906a4f8172
Ismael Figueroa <ifigueroap@gmail.com>
2019-10-29 20:44:10 -0300

    Se agrega funcionalidad hdp_test_io

--- hdptest/__init__.py
+++ hdptest/__init__.py
@@ -7,9 +7,17 @@ def hdptest(line, cell):
 
 del hdptest
 
+@register_cell_magic
+def hdptest_io(line, cell):
+    get_ipython().run_cell_magic('run_pytest[clean]', ' -qq --capture=sys --disable-warnings', cell)
+
+del hdptest_io
+
+
 import ipytest
 import pytest
 ipytest.config(rewrite_asserts=True, magics=True, tempfile_fallback=True)
 
 from .hdptest import hdp_test
+from .hdptest import hdp_test_io    
 
